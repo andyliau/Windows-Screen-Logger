b62b6152c2943c313307f0c8e90fdbbff5804e24
Andy Li <andy.li@wisetechglobal.com>
2025-01-07 20:53:12 +1030

    bigger setting UI

--- WindowsScreenLogger/SettingsForm.cs
+++ WindowsScreenLogger/SettingsForm.cs
@@ -24,44 +24,48 @@ public partial class SettingsForm : Form
 		// label1
 		// 
 		this.label1.AutoSize = true;
-		this.label1.Location = new System.Drawing.Point(13, 13);
+		this.label1.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+		this.label1.Location = new Point(20, 20);
 		this.label1.Name = "label1";
-		this.label1.Size = new System.Drawing.Size(147, 15);
+		this.label1.Size = new Size(220, 21);
 		this.label1.TabIndex = 0;
 		this.label1.Text = "Recording Interval (seconds):";
 		// 
 		// numericUpDownInterval
 		// 
-		this.numericUpDownInterval.Location = new System.Drawing.Point(166, 11);
+		this.numericUpDownInterval.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+		this.numericUpDownInterval.Location = new Point(250, 18);
 		this.numericUpDownInterval.Minimum = new decimal(new int[] {
-		1,
-		0,
-		0,
-		0});
+			1,
+			0,
+			0,
+			0});
 		this.numericUpDownInterval.Name = "numericUpDownInterval";
-		this.numericUpDownInterval.Size = new System.Drawing.Size(120, 23);
+		this.numericUpDownInterval.Size = new Size(120, 29);
 		this.numericUpDownInterval.TabIndex = 1;
 		this.numericUpDownInterval.Value = new decimal(new int[] {
-		5,
-		0,
-		0,
-		0});
+			5,
+			0,
+			0,
+			0});
 		// 
 		// checkBoxStartWithWindows
 		// 
 		this.checkBoxStartWithWindows.AutoSize = true;
-		this.checkBoxStartWithWindows.Location = new System.Drawing.Point(13, 41);
+		this.checkBoxStartWithWindows.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+		this.checkBoxStartWithWindows.Location = new Point(20, 60);
 		this.checkBoxStartWithWindows.Name = "checkBoxStartWithWindows";
-		this.checkBoxStartWithWindows.Size = new System.Drawing.Size(127, 19);
+		this.checkBoxStartWithWindows.Size = new Size(160, 25);
 		this.checkBoxStartWithWindows.TabIndex = 2;
 		this.checkBoxStartWithWindows.Text = "Start with Windows";
 		this.checkBoxStartWithWindows.UseVisualStyleBackColor = true;
 		// 
 		// buttonSave
 		// 
-		this.buttonSave.Location = new System.Drawing.Point(13, 67);
+		this.buttonSave.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+		this.buttonSave.Location = new Point(100, 100);
 		this.buttonSave.Name = "buttonSave";
-		this.buttonSave.Size = new System.Drawing.Size(75, 23);
+		this.buttonSave.Size = new Size(100, 30);
 		this.buttonSave.TabIndex = 3;
 		this.buttonSave.Text = "Save";
 		this.buttonSave.UseVisualStyleBackColor = true;
@@ -69,9 +73,10 @@ public partial class SettingsForm : Form
 		// 
 		// buttonCancel
 		// 
-		this.buttonCancel.Location = new System.Drawing.Point(95, 67);
+		this.buttonCancel.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+		this.buttonCancel.Location = new Point(220, 100);
 		this.buttonCancel.Name = "buttonCancel";
-		this.buttonCancel.Size = new System.Drawing.Size(75, 23);
+		this.buttonCancel.Size = new Size(100, 30);
 		this.buttonCancel.TabIndex = 4;
 		this.buttonCancel.Text = "Cancel";
 		this.buttonCancel.UseVisualStyleBackColor = true;
@@ -79,13 +84,19 @@ public partial class SettingsForm : Form
 		// 
 		// SettingsForm
 		// 
-		this.ClientSize = new System.Drawing.Size(300, 100);
+		this.AutoScaleDimensions = new SizeF(7F, 15F);
+		this.AutoScaleMode = AutoScaleMode.Font;
+		this.ClientSize = new Size(400, 150);
 		this.Controls.Add(this.buttonCancel);
 		this.Controls.Add(this.buttonSave);
 		this.Controls.Add(this.checkBoxStartWithWindows);
 		this.Controls.Add(this.numericUpDownInterval);
 		this.Controls.Add(this.label1);
+		this.FormBorderStyle = FormBorderStyle.FixedDialog;
+		this.MaximizeBox = false;
+		this.MinimizeBox = false;
 		this.Name = "SettingsForm";
+		this.StartPosition = FormStartPosition.CenterScreen;
 		this.Text = "Settings";
 		this.Load += new EventHandler(this.SettingsForm_Load);
 		((System.ComponentModel.ISupportInitialize)(this.numericUpDownInterval)).EndInit();
